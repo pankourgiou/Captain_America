aa9d4739a94ccf876587d0baa66093f29471b560
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-10-31 09:35:02 +0200

    Create RAM.py

---
+++ RAM.py
@@ -0,0 +1,12 @@
+import time
+
+
+t1 = time.time()
+
+for _ in range(2000):
+    print("xεΝ = {1, 2, 3, ....} = xEN = sheep")
+
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
