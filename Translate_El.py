7e57d2d43bf94260ee4fa99cc682f537e92631a0
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-12-31 03:46:50 +0200

    Update Translate_El.py

--- Translate_El.py
+++ Translate_El.py
@@ -1,4 +1,31 @@
 #Install translators module
+#The alphabet sound from A-Z are:
+#sa
+#be
+#ce
+#de
+#e
+#fe
+#ge
+#ha
+#i
+#je
+#ka
+#le
+#me
+#ne
+#o
+#pe
+#qe
+#re
+#se
+#te
+#u
+#ve
+#we
+#xe
+#ye
+#ze
 import translators as ts
 
 text = 'cheese'
